84fea46cd659ae586a8de771755ef781e5f6837f
nilsec <nils.eckstein@googlemail.com>
2021-06-23 06:44:23 -0400

    Update description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='disc',
     version='0.1',
-    description='Fade',
+    description='Disc',
     url='https://github.com/nilsec/disc',
     author='Nils Eckstein',
     packages=[
